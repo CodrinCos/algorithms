def23d4bbc2036fa8643b6431ceec3757fca4343
Codrin Costea <codrin.costea22@gmail.com>
2024-04-18 12:16:59 +0200

    solution to the problem

--- Program.cs
+++ Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("");
+﻿int GetMaxContainerArea(int[] nums)
+{
+    var indexLeft = 0;
+    var indexRight = nums.Length - 1;
+    var maxArea = 0;
+
+    while (indexLeft != indexRight)
+    {
+        var currentArea = int.Min(nums[indexLeft], nums[indexRight]) * (indexRight - indexLeft);
+        if (currentArea > maxArea)
+        {
+            maxArea = currentArea;
+        }
+
+        if (nums[indexLeft] == int.Min(nums[indexLeft], nums[indexRight]))
+        {
+            indexLeft++;
+        }
+        else
+        {
+            indexRight--;
+        }
+    }
+
+    return maxArea;
+}
+
+Console.WriteLine("Given an array of positive int, each int is a height of vertical line");
+Console.WriteLine("Find 2 lines which together with the x axis forms a container that would hold the greatest amount");
+Console.WriteLine("of water.Return the area of water it would hold.");
+
+var maxContainerArea = GetMaxContainerArea([1, 2, 10, 7, 2, 3, 6, 5, 4, 3]);
+
+Console.WriteLine(maxContainerArea.ToString());
