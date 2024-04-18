e6a27f3e971165f6c20e77b284658de50389688c
Codrin Costea <codrin.costea22@gmail.com>
2024-04-18 09:03:29 +0200

    given an array find 2 item that sum up to a given value

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿static int[] FindTwoSum(int[] nums, int target)
+{
+    var numsMap = new Dictionary<int, int>();
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (numsMap.TryGetValue(nums[i], out int previousValue))
+        {
+            return [previousValue, i];
+        }
+        else
+        {
+            var numberToFind = target - nums[i];
+            numsMap[numberToFind] = i;
+        }
+    }
+
+    return [];
+}
+
+Console.WriteLine("This solves Two sum: given an array of int and a value, find the 2 indexes from array that their values summed returns the value");
+
+var toReturn = FindTwoSum([1, 3, 7, 9, 10], 11);
+
+if (toReturn.Length == 0) Console.WriteLine("Not found");
+
+if (toReturn.Length == 2) Console.WriteLine($"{toReturn[0]}, {toReturn[1]}");
+
+
