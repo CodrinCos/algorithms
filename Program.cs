34f8e534fe392a0a33a7064b48f6cc6a261807de
Codrin Costea <codrin.costea22@gmail.com>
2024-04-24 08:33:23 +0200

    solution from geeksforgeeks - working

--- Program.cs
+++ Program.cs
@@ -1,56 +1,53 @@
-﻿bool areEqual(string s, string t)
-{
-    var p1 = s.ToArray().Length - 1;
-    var p2 = t.ToArray().Length - 1;
-    while (p1 >= 0 || p2 >= 0)
-    {
-        if (s.ToArray()[p1] == '#' || t.ToArray()[p2] == '#')
-        {
-            if (s.ToArray()[p1] == '#')
-            {
-                var backCount = 2;
-                while (backCount > 0)
-                {
-                    p1--;
-                    backCount--;
-                    if (s.ToArray()[p1] == '#')
-                    {
-                        backCount += 2;
-                    }
-                }
-            }
+﻿// C# implementation to Check if 
+// two strings after processing 
+// backspace character are equal or not
 
-            if (t.ToArray()[p2] == '#')
+using System;
+using System.Text;
+
+public class BackspaceStringCompare
+{
+
+    // function to remove backspaces and return refined string
+    static string RemoveBackspace(string str)
+    {
+        StringBuilder res = new StringBuilder();
+        foreach (char c in str)
+        {
+            if (c != '#')
             {
-                var backCount = 2;
-                while (backCount > 0)
-                {
-                    p2--;
-                    backCount--;
-                    if (s.ToArray()[p2] == '#')
-                    {
-                        backCount += 2;
-                    }
-                }
+                res.Append(c);
             }
+            else if (res.Length > 0)
+            {
+                res.Remove(res.Length - 1, 1);
+            }
+        }
+        return res.ToString();
+    }
+
+    // function to compare the two strings
+    static bool Compare(string s, string t)
+    {
+        s = RemoveBackspace(s);
+        t = RemoveBackspace(t);
+        return s.Equals(t);
+    }
+
+    // Driver code
+    public static void Main(string[] args)
+    {
+        string s = "asdf#e#ks";
+        string t = "aasdf#e#ks";
+        if (Compare(s, t))
+        {
+            Console.WriteLine("True");
         }
         else
         {
-            if (s.ToArray()[p1] != t.ToArray()[p2])
-            {
-                return false;
-            }
-            else
-            {
-                p1--;
-                p2--;
-            }
+            Console.WriteLine("False");
         }
     }
-
-    return true;
 }
 
-Console.WriteLine("Typed out strings - easy");
-
-Console.WriteLine(areEqual("aasdf#a", "asdx#a"));
+// This code is contributed by codebraxnzt
