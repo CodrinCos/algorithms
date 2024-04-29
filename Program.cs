cc3c1a0428ae8c89b4eb5cd89441a5b6c9bc26d8
Codrin Costea <codrin.costea22@gmail.com>
2024-04-29 08:07:00 +0200

    added solution

--- Program.cs
+++ Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("");
+﻿int getMaxSubstring(string s)
+{
+    if (s.Length <= 1)
+    {
+        return s.Length;
+    }
+
+    var seenChar = new Dictionary<char, int>();
+    var left = 0;
+    var longest = 0;
+
+    for (var right = 0; right < s.Length; right++)
+    {
+        var currentChar = s[right];
+        int prevSeenChar = -1;
+        if (seenChar.TryGetValue(currentChar, out prevSeenChar) && prevSeenChar >= left)
+        {
+            left = prevSeenChar + 1;
+        }
+
+        if (!seenChar.TryAdd(currentChar, right))
+        {
+            seenChar.Remove(currentChar);
+            seenChar.Add(currentChar, right);
+        }
+
+        longest = Math.Max(longest, right - left + 1);
+    }
+
+    return longest;
+}
+
+Console.WriteLine("Longest substring");
+
+Console.WriteLine(getMaxSubstring("asdfgadsa"));
