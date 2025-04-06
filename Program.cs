3a1d7896e85b4311a6301562176f1df86512370c
CodrinCos <codrin.costea22@gmail.com>
2025-04-06 18:18:28 +0200

    added implementation

--- Program.cs
+++ Program.cs
@@ -1,35 +1,30 @@
-﻿Console.WriteLine("Binary search with recursion...");
+﻿Console.WriteLine("Bubble sort...");
 
 Console.WriteLine("Add numbers: ");
 int[] sortedNumbers = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
 
-Console.WriteLine("I want to find: ");
-int numberToFind = Convert.ToInt32(Console.ReadLine());
+BubbleSort(sortedNumbers);
 
-bool isPresent = BinarySearch(sortedNumbers, 0, sortedNumbers.Length - 1, numberToFind);
-if (isPresent)
+static void BubbleSort(int[] arr)
 {
-    Console.WriteLine($"{numberToFind} is present in the array.");
-}
-else
-{
-    Console.WriteLine($"{numberToFind} is not present in the array.");
-}
-
-static bool BinarySearch(int[] arr, int left, int right, int x)
-{
-    if (right >= left)
+    int n = arr.Length;
+    for (int i = 0; i < n - 1; i++)
     {
-        int mid = left + (right - left) / 2;
-        // If the element is present at the middle itself
-        if (arr[mid] == x)
-            return true;
-        // If the element is smaller than mid, then it can only be present in left subarray
-        if (arr[mid] > x)
-            return BinarySearch(arr, left, mid - 1, x);
-        // Else the element can only be present in right subarray
-        return BinarySearch(arr, mid + 1, right, x);
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                // swap arr[j] and arr[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
     }
-    // Element is not present in array
-    return false;
+
+    Console.WriteLine("Sorted array: ");
+    foreach (var item in arr)
+    {
+        Console.Write(item + " ");
+    }
 }
